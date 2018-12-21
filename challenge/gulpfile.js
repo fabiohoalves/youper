101cff1bd68a7d336b6016868b3236d8060a7709
Fabio Alves <fabioh80@gmail.com>
2018-12-21 11:51:13 -0200

    added compile to typescript

--- challenge/gulpfile.js
+++ challenge/gulpfile.js
@@ -2,12 +2,23 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var typescript = require('gulp-tsc');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  src: ['./src/*.ts']
 };
 
 gulp.task('default', ['sass']);
+
+gulp.task('compile', function(){
+  gulp.src(paths.src)
+  .pipe(typescript({
+     emitError: false
+  }))
+  .pipe(gulp.dest('www/js/'))
+
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -24,4 +35,5 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', ['sass'], function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.src, ['compile']);
 });
